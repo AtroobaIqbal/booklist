e4211e6c7be499cafc0b10f6fb91afc03cd7a4b6
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2024-01-13 19:53:55 +0500

    form complete but have some errors to connecting js file

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-// search btn
+//   search btn
+
 searchform = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchform.classList.toggle("active");
+}
+
+//    login form
+
+var loginform = document.querySelector(".login-form-container");
+document.querySelector("#login-btn").onclick = () => {
+    loginform.classList.toggle("active")
+}
+
+document.querySelector("#close-login-btn").onclick = () => {
+    loginform.classList.remove("active");
 }
