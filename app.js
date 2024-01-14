35dadab0e3ad128658f23744835e4f9cf4c47bfc
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2024-01-14 18:08:55 +0500

    add console

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 searchform = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchform.classList.toggle("active");
+    console.log("search form");
 }
 
 //    login form
 
 var loginform = document.querySelector(".login-form-container");
 document.querySelector("#login-btn").onclick = () => {
-    loginform.classList.toggle("active")
+    loginform.classList.toggle("active");
+    console.log("log in form");
 }
 
 document.querySelector("#close-login-btn").onclick = () => {
     loginform.classList.remove("active");
+    console.log("log in remove");
 }
