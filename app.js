bdf77d2d0dcfdc0c3339496507e4d0e39c432931
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2024-01-15 20:29:53 +0500

    home section complete

--- app.js
+++ app.js
@@ -18,3 +18,25 @@ document.querySelector("#close-login-btn").onclick = () => {
     loginform.classList.remove("active");
     console.log("log in remove");
 }
+
+//     home section
+
+var swipper= new Swiper('.books-list' , {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1040: {
+            slidesPerView: 3
+        },
+    },
+});
